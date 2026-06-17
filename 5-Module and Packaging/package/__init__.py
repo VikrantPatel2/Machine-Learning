5142a6a7ca67e2245beede0d7b9f97c20bb38899
Vikrant Patel <148195821+VikrantPatel2@users.noreply.github.com>
2026-06-17 17:02:54 +0530

    Add files via upload

---
+++ 5-Module and Packaging/package/__init__.py
@@ -0,0 +1,3 @@
+'''
+This file is important to import package if you are making your own package
+'''
---
+++ 5-Module and Packaging/test.py
@@ -0,0 +1,5 @@
+from package.maths import *
+from package.subpackage.mult import multiply
+print(addition(2,3))
+print(substaction(3,1))
+print(multiply(2,3))
